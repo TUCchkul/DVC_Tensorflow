2aa34dbc2a80ade5844af32dcaffca945cb029da
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-04-19 06:40:38 +0200

    Stage _01 updated

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -4,19 +4,26 @@ import pandas as pd
 import os
 import shutil
 from tqdm import tqdm
+import logging 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
+
 def copy_file(source_download_dir,local_data_dir):
     list_of_files=os.listdir(source_download_dir)
     N=len(list_of_files)
-    for file in tqdm(list_of_files, total=N, desc=f"copying file from {source_download_dir} to {local_data_dir}", color="green"):
+    for file in tqdm(list_of_files, total=N, desc=f'copying file from {source_download_dir} to {local_data_dir}', colour="green"):
         src=os.path.join(source_download_dir,file)
         dest=os.path.join(local_data_dir,file)
         shutil.copy(src,dest)
 def get_data(config_path):
     config = read_yaml(config_path)
     source_download_dirs=config["source_download_dirs"]
-    local_data_dirs=config["ocal_data_dirs"]
+    local_data_dirs=config["local_data_dirs"]
 
-    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc="list of folder", color="red"):
+    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc="list of folder", colour="red"):
         create_directory([local_data_dir])
         copy_file(source_download_dir,local_data_dir)
 
@@ -29,4 +36,10 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-    get_data(config_path=parsed_args.config)
+    try:
+        logging.info(">>>>> stage one started")
+        get_data(config_path=parsed_args.config)
+        logging.info("Stage one completed!!!!!<<<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
